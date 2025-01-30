3ff81988e7412256e4ffe309500af23b5cd0d5f7
Jéssica Rodrigues <jessicarodridc@gmail.com>
2025-01-29 22:04:54 -0300

    Add class repository and persistence

---
+++ src/main/java/br/com/fiap/techchallenge/payment/infra/gateway/database/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package br.com.fiap.techchallenge.payment.infra.gateway.database.repository;
+
+import br.com.fiap.techchallenge.payment.infra.gateway.database.entities.PaymentEntity;
+import java.util.*;
+import java.time.LocalDateTime;
+
+public interface PaymentRepository {
+
+	PaymentEntity save(PaymentEntity paymentEntity);
+
+	Optional<PaymentEntity> findByOrderId(UUID orderId);
+
+	Optional<PaymentEntity> findByExternalPaymentId(UUID externalPaymentId);
+
+	List<PaymentEntity> findByPaidIsFalseAndCreatedAtBefore(LocalDateTime createdAt);
+
+}
---
+++ src/main/java/br/com/fiap/techchallenge/payment/infra/gateway/database/repository/impl/PaymentRepositoryImpl.java
@@ -0,0 +1,73 @@
+package br.com.fiap.techchallenge.payment.infra.gateway.database.repository.impl;
+
+import br.com.fiap.techchallenge.payment.infra.gateway.database.entities.PaymentEntity;
+import br.com.fiap.techchallenge.payment.infra.gateway.database.repository.PaymentRepository;
+import io.awspring.cloud.dynamodb.DynamoDbTemplate;
+import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+	private final DynamoDbTemplate dynamoDbTemplate;
+
+	public PaymentRepositoryImpl(DynamoDbTemplate dynamoDbTemplate) {
+		this.dynamoDbTemplate = dynamoDbTemplate;
+	}
+
+	@Override
+	public PaymentEntity save(PaymentEntity paymentEntity) {
+		dynamoDbTemplate.save(paymentEntity);
+		return paymentEntity;
+	}
+
+	@Override
+	public Optional<PaymentEntity> findByOrderId(UUID orderId) {
+		ScanEnhancedRequest scanEnhancedRequest = ScanEnhancedRequest.builder()
+			.filterExpression(Expression.builder()
+				.expression("orderId = :orderIdVal")
+				.expressionValues(Map.of(":orderIdVal", AttributeValue.builder().s(orderId.toString()).build()))
+				.build())
+			.build();
+
+		var history = dynamoDbTemplate.scan(scanEnhancedRequest, PaymentEntity.class);
+		return history.items().stream().findFirst();
+	}
+
+	@Override
+	public Optional<PaymentEntity> findByExternalPaymentId(UUID externalPaymentId) {
+		ScanEnhancedRequest scanEnhancedRequest = ScanEnhancedRequest.builder()
+			.filterExpression(Expression.builder()
+				.expression("externalPaymentId = :externalPaymentIdVal")
+				.expressionValues(Map.of(":externalPaymentIdVal",
+						AttributeValue.builder().s(externalPaymentId.toString()).build()))
+				.build())
+			.build();
+
+		var history = dynamoDbTemplate.scan(scanEnhancedRequest, PaymentEntity.class);
+		return history.items().stream().findFirst();
+	}
+
+	@Override
+	public List<PaymentEntity> findByPaidIsFalseAndCreatedAtBefore(LocalDateTime createdAt) {
+		ScanEnhancedRequest scanEnhancedRequest = ScanEnhancedRequest.builder()
+			.filterExpression(Expression.builder()
+				.expression("paid = :paidVal AND createdAt < :createdAtVal")
+				.expressionValues(Map.of(":paidVal", AttributeValue.builder().bool(false).build(), ":createdAtVal",
+						AttributeValue.builder().s(createdAt.toString()).build()))
+				.build())
+			.build();
+
+		var history = dynamoDbTemplate.scan(scanEnhancedRequest, PaymentEntity.class);
+		return history.items().stream().toList();
+	}
+
+}
