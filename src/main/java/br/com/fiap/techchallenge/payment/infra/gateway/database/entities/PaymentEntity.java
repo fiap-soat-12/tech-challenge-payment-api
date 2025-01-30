0e3590d0d87366be87a8090b1cd6887dbd52e166
Jéssica Rodrigues <jessicarodridc@gmail.com>
2025-01-29 22:01:02 -0300

    Add class PaymentEntity, domain Payment

---
+++ src/main/java/br/com/fiap/techchallenge/payment/infra/gateway/database/entities/PaymentEntity.java
@@ -0,0 +1,125 @@
+package br.com.fiap.techchallenge.payment.infra.gateway.database.entities;
+
+import br.com.fiap.techchallenge.payment.domain.models.Payment;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+
+@DynamoDbBean
+public class PaymentEntity {
+
+	private UUID id;
+
+	private BigDecimal amount;
+
+	private boolean isPaid;
+
+	private UUID externalPaymentId;
+
+	private String qr;
+
+	private UUID orderId;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	public PaymentEntity() {
+	}
+
+	public PaymentEntity(Payment payment) {
+		this.id = payment.getId();
+		this.amount = payment.getAmount();
+		this.isPaid = payment.isPaid();
+		this.externalPaymentId = payment.getExternalPaymentId();
+		this.qr = payment.getQr();
+		this.orderId = payment.getOrderId();
+		this.createdAt = payment.getCreatedAt();
+		this.updatedAt = payment.getUpdatedAt();
+	}
+
+	public PaymentEntity update(Payment payment) {
+		this.id = payment.getId();
+		this.amount = payment.getAmount();
+		this.isPaid = payment.isPaid();
+		this.externalPaymentId = payment.getExternalPaymentId();
+		this.qr = payment.getQr();
+		this.orderId = payment.getOrderId();
+		this.createdAt = payment.getCreatedAt();
+		this.updatedAt = LocalDateTime.now();
+		return this;
+	}
+
+	public Payment toPayment() {
+		return new Payment(id, amount, isPaid, externalPaymentId, qr, orderId, createdAt, updatedAt);
+	}
+
+	@DynamoDbPartitionKey
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public boolean isPaid() {
+		return isPaid;
+	}
+
+	public void setPaid(boolean isPaid) {
+		this.isPaid = isPaid;
+	}
+
+	public UUID getExternalPaymentId() {
+		return externalPaymentId;
+	}
+
+	public void setExternalPaymentId(UUID externalPaymentId) {
+		this.externalPaymentId = externalPaymentId;
+	}
+
+	public String getQr() {
+		return qr;
+	}
+
+	public void setQr(String qr) {
+		this.qr = qr;
+	}
+
+	public UUID getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(UUID orderId) {
+		this.orderId = orderId;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
